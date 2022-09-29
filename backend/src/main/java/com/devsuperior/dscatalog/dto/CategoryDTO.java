4e2b8ddc5d9bf609b18db9e3454a7b4361ad3491
MarcioSill <marcio.sill@hotmail.com>
2022-09-28 21:55:53 -0300

    Product repository, service. resource, Get ok

--- backend/src/main/java/com/devsuperior/dscatalog/dto/CategoryDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/CategoryDTO.java
@@ -11,9 +11,6 @@ public class CategoryDTO implements Serializable {
 	private Long id;
 	private String name;
 	
-	public CategoryDTO() {
-	}
-
 	public CategoryDTO(Long id, String name) {
 		this.id = id;
 		this.name = name;
