17bb20fa989ca2b17c9e678c2b0a1d5a7cbf4416
MarcioSill <marcio.sill@hotmail.com>
2022-10-28 21:29:56 -0300

    Refactor pageable

--- backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
+++ backend/src/main/java/com/devsuperior/dscatalog/dto/ProductDTO.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
-
 import java.util.Set;
 
 import com.devsuperior.dscatalog.entities.Category;
